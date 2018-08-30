37980c0a27d1921fff8ad39e5afe5662b1f7c565
vieness <you@example.com>
2018-08-30 12:24:31 +0300

    fin

---
+++ form/src/ui.js
@@ -0,0 +1,112 @@
+class UI {
+    constructor(){
+        this.post = document.querySelector('#posts');
+        this.titleInput = document.querySelector('#title')
+        this.bodyInput = document.querySelector('#body')
+        this.idInput = document.querySelector('#id')
+        this.postInput = document.querySelector('.post-submit')
+        this.forState = 'add'
+    }
+    // Show all posts
+    showPosts(posts){
+        let output = '';
+
+        posts.forEach((post) => {
+            output += `
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h4 class="card-title">${post.title}</h4>
+                    <p class="card-text">${post.body}</p>
+                    <a href="#" class="edit card-link" data-id="${post.id}">
+                    <i class="fas fa-pencil-alt"></i>  
+                    </a>
+                    <a href="#" class="delete card-link" data-id="${post.id}"><i class="fas fa-times"></i> </a>
+                </div>
+            </div>
+            `
+        });
+
+        this.post.innerHTML = output;
+    }
+    // Show alert message
+    showAlert(message, className){
+        this.clearAlert();
+
+        // Create div
+        const div = document.createElement('div');
+        // Add classes
+        div.className = className;
+        // Add text
+        div.appendChild(document.createTextNode(message));
+        // Get parent
+        const container = document.querySelector('.postsContainer');
+        // Get posts
+        const posts = document.querySelector('#posts');
+        // Insert Alert div
+        container.insertBefore(div, posts);
+        // Timeout
+        setTimeout(() => {
+            this.clearAlert()
+        }, 3000);
+    }
+    // Clear alert message
+    clearAlert(){
+        const currentAlert = document.querySelector('.alert');
+
+        if(currentAlert){
+            currentAlert.remove()
+        }
+    }
+    // Clear all fields
+    clearFields(){
+        this.titleInput.value = '';
+        this.bodyInput.value = '';
+    }
+
+    // Fill form to edit
+    fillForm(data){
+        this.titleInput.value = data.title;
+        this.bodyInput.value = data.body;
+        this.idInput.value = data.id;
+
+        this.changeFormState('edit')
+    }
+
+    // Clear Id input
+    clearIdInput(){
+        this.idInput.value = '';
+    }
+
+    // Change the form state
+    changeFormState(type){
+        if(type === 'edit'){
+            this.postInput.textContent = 'Редагувати пост'
+            this.postInput.className = 'post-submit btn btn-warning btn-block';
+
+            // Create cancel button
+            const button = document.createElement('button');
+            button.className = 'post-cancel btn btn-light btn-block'
+            button.appendChild(document.createTextNode('Відмінити редагування'));
+
+            // Get parent
+            const cardForm = document.querySelector('.card-form');
+            // Get element to insert before
+            const formEnd = document.querySelector('.form-end');
+            // Insert cancel btn
+            cardForm.insertBefore(button, formEnd);
+        } else{
+            this.postInput.textContent = 'Опублікувати'
+            this.postInput.className = 'post-submit btn btn-primary btn-block';
+            // Remove cancel btn if it is there
+            if(document.querySelector('.post-cancel')){
+                document.querySelector('.post-cancel').remove();
+            }
+            // Clear ID from hidden field
+            this.clearIdInput();
+            // Clear Text
+            this.clearFields();
+        }
+    }
+}
+
+export const ui = new UI();
